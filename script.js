8df05e7004f5ca53af93483a084a80c6c63af0a4
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-05-01 12:47:58 +0200

    spread/rest done

--- script.js
+++ script.js
@@ -55,9 +55,7 @@ const singlePerson = {
 console.log(singlePerson)
 
 
-
-
-// Destructuración
+//* Destructuración
 
 const singleDog = {
   name: "Jake",
@@ -77,7 +75,7 @@ const otherDog = {
 // Destructuración es extraer las propiedades de un objeto como variables para facil uso.
 // Destructuración NO muta el objeto original
 
-// syntaxis
+//? syntaxis
 // const { *propiedades a extraer* } = *objeto donde estan las propiedades*
 const { dogAge, name, color  } = singleDog
 // estoy creando variables con los valores de las propiedades
@@ -104,7 +102,7 @@ dogMessage(otherDog)
 
 
 
-// Destructuración en Arrays
+//* Destructuración en Arrays
 
 const myTopFavouriteBooks = [ "Harry Potter", "Discworld", "Dresden Files", "LotR", "Dragonlance", "Star Wars" ]
 
@@ -122,7 +120,7 @@ console.log( `Mis libros favoritos son: ${book1}, ${book2}, ${book3}, ${book4} y
 console.log(book6)
 
 
-// Destructuración compleja
+//* Destructuración compleja
 
 
 
@@ -153,3 +151,112 @@ console.log(quantity)
 // destructurar la segunda peli de Chriss Pratt
 const { other: { films: { top: [, peli2] } } } = someone
 console.log(peli2)
+
+
+//? syntaxis
+// const { *propiedadObj*: { *propiedad anidada* } } = *objeto padre*
+
+
+
+//* Actividad 3
+
+// Given the object, use function parameter destructuring to get the following output:
+
+const greenSmoothie = {
+  leaf: 'spinach',
+  veggie: 'bok choy',
+  seed: 'ground flex seeds',
+  nut: 'peanut',
+  liquid: 'almond milk'
+};
+
+// destructure the object in the parameters itself
+function getIngredients( { leaf, veggie, seed, nut, liquid } ) {
+  // const { leaf, veggie, seed, nut, liquid } = obj
+
+  return `To make the green smoothie,  you  should add: ${leaf}, ${veggie}, ${seed}, ${nut} and ${liquid}.`;
+}
+
+// ensure you pass the object as a parameter while calling the function below
+console.log( getIngredients( greenSmoothie ) )
+
+// => expected Output: 
+// To make a green smoothie, you should add: spinach, bok choy, ground flex seeds, peanut and almond milk.
+
+
+
+
+//* Operador Spread (...) => Esparcir
+
+
+const someNumbers = [ 10, 20, 100, 8, 14 ]
+
+console.log( someNumbers )
+console.log( ...someNumbers ) // esparciendo los elementos del array
+console.log( 10, 20, 100, 8, 14 ) // ES LO MISMO QUE ARRIBA
+
+// Primer uso => metodos que requieren varios valores (pero no un array)
+console.log( Math.max( 1, 10, 1000, 40 ) )
+console.log( Math.max( ...someNumbers ) )
+
+
+// Segundo uso => clonar arrays
+
+const numbersCopy = [...someNumbers] // nuevo array
+
+numbersCopy.reverse()
+console.log(numbersCopy)
+console.log("original", someNumbers)
+
+// Tercer uso => crear arrays con elementos de otros
+
+const students = [ "dani", "alejandro", "jonathan" ];
+const staff = [ "caro", "ruth", "clara" ]
+
+const newArr = [ ...students, ...staff.slice(0,2), "alvaro", "santi" ]
+console.log(newArr)
+
+
+
+//* Operador REST (...) => El resto de los elementos
+
+//* Primer uso => obtener el resto de elementos de una destructuración de arrays
+
+const hobbies = [ "surfear", "videojuegos", "leer", "escalar", "ver pelis" ]
+
+const [ hobby1, hobby2, ...otherHobbies ] = hobbies
+
+console.log(hobby1, hobby2)
+
+// quiero EL RESTO de los elementos de ese array
+console.log(otherHobbies)
+
+//* Segundo usop => obtener el resto de los argumentos pasados a una función.
+
+
+// 1. Un valor total
+// 2. Un valor a sumar
+// 3. Otro valor a sumar
+
+function checkAmount(total, ...allNumbers) {
+
+  console.log(allNumbers)
+
+  let sum = allNumbers.reduce((acc, elem) => {
+    return acc + elem
+  }, 0)
+
+
+  if (sum === total) {
+    return `la suma es exactamente ${total}`
+  } else {
+    return `la suma no da el total ${total}`
+  }
+
+}
+
+console.log( checkAmount(12, 4, 8) ) 
+console.log( checkAmount(15, 10, 4, 1) ) 
+console.log( checkAmount(20, 2, 2, 2, 2, 2, 9) ) 
+
+
